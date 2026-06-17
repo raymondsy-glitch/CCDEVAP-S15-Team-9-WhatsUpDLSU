09bd1a06839bcb1cae13958112fd2e2d591333d4
leftythebestcoder <phil_tan@dlsu.edu.ph>
2026-06-17 18:01:59 +0800

    Enhance login functionality with redirection
    Updated login form submission to redirect based on the current page.

--- login-side-main/js/script.js
+++ login-side-main/js/script.js
@@ -14,5 +14,21 @@ toggleButton.addEventListener("click",() => {
 
 document.getElementById("loginForm").addEventListener("submit", (event) => {
     event.preventDefault();
-    alert("Login submitted.");
+    alert("Login successful.");
+    const page = window.location.pathname.split("/").pop();
+
+    let redirectPath;
+    switch (page) {
+        case "index.html":
+            redirectPath = "../student-side-main/dashboard.html";
+            break;
+        case "admin-login.html":
+            redirectPath = "../admin-side-main/dashboard.html";
+            break;
+        case "officer-login.html":
+            redirectPath = "../org-side-main/dashboard.html";
+            break;
+    }
+
+    window.location.href = redirectPath;
 });
